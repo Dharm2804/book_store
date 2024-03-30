dfc075c70e721b535fb68a1ecec41265ea26f78c
Dharm2804 <125880807+Dharm2804@users.noreply.github.com>
2024-03-30 18:13:57 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('https://book-store-gcen.onrender.com')
+      .get('https://book-store-gcen.onrender.com/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
